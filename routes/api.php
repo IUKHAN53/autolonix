53874b620c5739b168b888d5769a9f0d6fdabd4c
Irfan Ullah <iukhan53@gmail.com>
2023-07-15 20:24:36 +0500

    completed backend

--- routes/api.php
+++ routes/api.php
@@ -3,35 +3,37 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\DrilldownController;
 use App\Http\Controllers\Api\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/me', [AuthController::class, 'me']);
-//    Products API
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/products/create', [ProductController::class, 'create']);
-    Route::post('/products', [ProductController::class, 'store']);
-    Route::get('/products/{id}', [ProductController::class, 'show']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 
-//    Categories API
-    Route::get('/categories', [DrilldownController::class, 'index']);
-    Route::post('/categories', [DrilldownController::class, 'store']);
-    Route::get('/categories/{id}', [DrilldownController::class, 'show']);
-    Route::put('/categories/{id}', [DrilldownController::class, 'update']);
-    Route::delete('/categories/{id}', [DrilldownController::class, 'destroy']);
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/create', [ProductController::class, 'create']);
+        Route::post('/', [ProductController::class, 'store']);
+        Route::get('/{id}', [ProductController::class, 'show']);
+        Route::put('/{id}', [ProductController::class, 'update']);
+        Route::delete('/{id}', [ProductController::class, 'destroy']);
+    });
 
-//    SubCategories API
-    Route::get('/subcategories', [ProductController::class, 'getSubCategories']);
-    Route::post('/subcategories', [DrilldownController::class, 'subcategoryStore']);
-    Route::get('/subcategories/{id}', [DrilldownController::class, 'show']);
-    Route::put('/subcategories/{id}', [DrilldownController::class, 'subcategoryUpdate']);
-    Route::delete('/subcategories/{id}', [DrilldownController::class, 'destroy']);
+    Route::prefix('categories')->group(function () {
+        Route::get('/', [DrilldownController::class, 'index']);
+        Route::post('/', [DrilldownController::class, 'store']);
+        Route::get('/{id}', [DrilldownController::class, 'show']);
+        Route::put('/{id}', [DrilldownController::class, 'update']);
+        Route::delete('/{id}', [DrilldownController::class, 'destroy']);
+    });
+
+    Route::prefix('subcategories')->group(function () {
+        Route::get('/', [ProductController::class, 'getSubCategories']);
+        Route::post('/', [DrilldownController::class, 'subcategoryStore']);
+        Route::get('/{id}', [DrilldownController::class, 'show']);
+        Route::put('/{id}', [DrilldownController::class, 'subcategoryUpdate']);
+        Route::delete('/{id}', [DrilldownController::class, 'destroy']);
+    });
 });
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
