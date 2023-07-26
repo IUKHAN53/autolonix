244490dca1e91d8dc04c2ec2157b9a023016ba48
Irfan Ullah <iukhan53@gmail.com>
2023-07-26 22:03:34 +0500

    worked on purchase

--- app/Traits/SetDefaultData.php
+++ app/Traits/SetDefaultData.php
@@ -7,18 +7,16 @@ trait SetDefaultData
 {
     protected static function booted()
     {
-        static::created(function ($model) {
+        static::creating(function ($model) {
             if(isset($model->attributesToArray()['cr_by'])) $model->cr_by = auth()->id();
             if(isset($model->attributesToArray()['cr_on'])) $model->cr_on = date('Y-m-d H:i:s');
             if(isset($model->attributesToArray()['upload_status_c'])) $model->upload_status_c = 0;
             $station_id = GlobalSettings::query()->where('type', 'station_id')->pluck('value')->first();
             if(isset($model->attributesToArray()['station_id'])) $model->station_id = $station_id;
-            $model->save();
         });
-        static::updated(function ($model) {
+        static::updating(function ($model) {
             if(isset($model->attributesToArray()['mod_by'])) $model->mod_by = auth()->id();
             if(isset($model->attributesToArray()['mod_on'])) $model->mod_on = date('Y-m-d H:i:s');
-            $model->save();
         });
     }
 }
